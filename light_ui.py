d4751e0278c02b4664f386b4c750c95a9aa2f75d
Ralph Scharpf <ralph.scharpf@gmail.com>
2017-07-09 15:05:52 +0200

    CHANGED: Switched to using events instead of properties as event are more versatile

--- light_ui.py
+++ light_ui.py
@@ -26,12 +26,12 @@ class LightUi(ui.Widget):
             with ui.GroupWidget(title='Lamp garden'):
                 with ui.VBox():
                     with ui.HBox():
-                        self.lampGardenButtonAuto = ui.RadioButton(text='auto', checked=True)
-                        self.lampGardenButtonOn   = ui.RadioButton(text='on')
-                        self.lampGardenButtonOff  = ui.RadioButton(text='off')
+                        self.lampTerraceButtonAuto = ui.RadioButton(text='auto', checked=True)
+                        self.lampTerraceButtonOn   = ui.RadioButton(text='on')
+                        self.lampTerraceButtonOff  = ui.RadioButton(text='off')
                     with ui.HBox():
                         ui.CheckBox(flex=0, text='Dim')
-                        self.lampGardenBrightnessSlider = ui.Slider(flex = 1, min=1, max = 100)
+                        self.lampTerraceBrightnessSlider = ui.Slider(flex = 1, min=1, max = 100)
             with ui.GroupWidget(title='Lamp garage'):
                 with ui.HBox():
                     self.lampGarageButtonAuto = ui.RadioButton(text='auto', checked = True)
@@ -51,19 +51,19 @@ class LightUi(ui.Widget):
                     self.detectorGarageButtonMasked = ui.RadioButton(text='masked')
             ui.Widget(flex=1)
             
-    @event.connect('lampYardFrontState')
-    def handle_changes_to_foo_in_python(self, *events):
-        print(events)
+    @event.connect('changeLampState')
+    def handleStateChange(self, *events):
         ev = events[-1]
-        if ev.new_value == 'auto': lightControl.setRelaisMode(Relais.LAMP_WEST, RelaisMode.Auto)
-        if ev.new_value == 'on':   lightControl.setRelaisMode(Relais.LAMP_WEST, RelaisMode.On)
-        if ev.new_value == 'off':  lightControl.setRelaisMode(Relais.LAMP_WEST, RelaisMode.Off)
-
-
-    class Both:
-        @event.prop
-        def lampYardFrontState(self, state='auto'):
-            return state
+        if ev.lamp == 'LampYardFront': relais = Relais.LAMP_WEST
+        if ev.lamp == 'LampYardRear':  relais = Relais.LAMP_SOUTH
+        if ev.lamp == 'LampTerrace':   relais = Relais.LAMP_TERRACE
+        if ev.lamp == 'LampGarage':    relais = Relais.LAMP_NORTH
+                
+        if ev.mode == 'on':   mode = RelaisMode.On
+        if ev.mode == 'off':  mode = RelaisMode.Off
+        if ev.mode == 'auto': mode = RelaisMode.Auto
+        
+        lightControl.setRelaisMode(relais, mode)
 
     class JS:
 
@@ -72,11 +72,16 @@ class LightUi(ui.Widget):
 #            ev = events[-1]
 #            self.buttonlabel.text = 'Clicked on the ' + ev.source.text
 
+        @event.emitter
+        def changeLampState(self, js_event):
+            return dict(lamp=js_event['lamp'], mode=js_event['mode'])
+
         @event.connect('lampYardFrontButtonAuto.checked', 
                        'lampYardFrontButtonOn.checked',
                        'lampYardFrontButtonOff.checked')
         def _radio_changed(self, *events):
-            self.lampYardFrontState = events[-1].source.text
+            ev = events[-1]
+            self.changeLampState({'lamp': 'LampYardFront', 'mode': ev.source.text})
 
 #        @event.connect('c1.checked', 'c2.checked','c3.checked',  )
 #        def _check_changed(self, *events):
