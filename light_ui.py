2c92061854c8abd2442cd609d9cc65541709c433
Ralph Scharpf <ralph.scharpf@gmail.com>
2017-07-10 16:55:28 +0200

    CHANGED: Flexx Events are now handled by Python class instead of JS class

--- light_ui.py
+++ light_ui.py
@@ -51,104 +51,85 @@ class LightUi(ui.Widget):
                     self.detectorGarageButtonMasked = ui.RadioButton(text='masked')
             ui.Widget(flex=1)
             
-    @event.connect('changeLampMode')
-    def handleLampModeChange(self, *events):
-        ev = events[-1]
-        if   ev.lamp == 'LampYardFront': relais = Relais.LAMP_WEST
-        elif ev.lamp == 'LampYardRear':  relais = Relais.LAMP_SOUTH
-        elif ev.lamp == 'LampTerrace':   relais = Relais.LAMP_TERRACE
-        elif ev.lamp == 'LampGarage':    relais = Relais.LAMP_NORTH
-        else: raise ValueError                
-        if   ev.mode == 'on':   mode = RelaisMode.On
-        elif ev.mode == 'off':  mode = RelaisMode.Off
-        elif ev.mode == 'auto': mode = RelaisMode.Auto
-        else: raise ValueError                
-        lightControl.setRelaisMode(relais, mode)
-
-    @event.connect('changeDetectorMode')
-    def handleDetectorModeChange(self, *events):
-        ev = events[-1]
-        if   ev.detector == 'DetectorYard':    detector = Detector.MOTION_SENSE_SOUTH
-        elif ev.detector == 'DetectorTerrace': detector = Detector.MOTION_SENSE_TERRACE
-        elif ev.detector == 'DetectorGarage':  detector = Detector.MOTION_SENSE_NORTH
+    def getRelaisModeFromText(self, text):
+        if   text == 'on':   mode = RelaisMode.On
+        elif text == 'off':  mode = RelaisMode.Off
+        elif text == 'auto': mode = RelaisMode.Auto
         else: raise ValueError
-        if   ev.mode == 'active':  mode = DetectorMode.Active
-        elif ev.mode == 'masked':  mode = DetectorMode.Masked
-        else: raise ValueError                
-        lightControl.setDetectorMode(detector, mode)
-        
-    @event.connect('changeBrightness')
-    def handleBrightnessChange(self, *events):
-        ev = events[-1]
-        if ev.lamp == 'LampTerrace':  lamp = Pwm.LAMP_TERRACE
+        return mode
+    
+    def getDetectorModeFromText(self, text):
+        if   text == 'active':  mode = DetectorMode.Active
+        elif text == 'masked':  mode = DetectorMode.Masked
         else: raise ValueError
-        lightControl.setPwm(lamp, ev.value)
+        return mode
+            
+    @event.connect('lampYardFrontButtonAuto.checked', 
+                   'lampYardFrontButtonOn.checked',
+                   'lampYardFrontButtonOff.checked')
+    def radioLampYardFrontChanged(self, *events):
+        ev = events[-1]
+        if (ev.new_value):
+            mode = self.getRelaisModeFromText(ev.source.text)
+            lightControl.setRelaisMode(Relais.LAMP_WEST, mode)
+            
+    @event.connect('lampYardRearButtonAuto.checked', 
+                   'lampYardRearButtonOn.checked',
+                   'lampYardRearButtonOff.checked')
+    def radioLampYardRearChanged(self, *events):
+        ev = events[-1]
+        if (ev.new_value):
+            mode = self.getRelaisModeFromText(ev.source.text)
+            lightControl.setRelaisMode(Relais.LAMP_SOUTH, mode)
+
+    @event.connect('lampTerraceButtonAuto.checked', 
+                   'lampTerraceButtonOn.checked',
+                   'lampTerraceButtonOff.checked')
+    def radioLampTerraceChanged(self, *events):
+        ev = events[-1]
+        if (ev.new_value):
+            mode = self.getRelaisModeFromText(ev.source.text)
+            lightControl.setRelaisMode(Relais.LAMP_TERRACE, mode)
+
+    @event.connect('lampGarageButtonAuto.checked', 
+                   'lampGarageButtonOn.checked',
+                   'lampGarageButtonOff.checked')
+    def radioLampGarageChanged(self, *events):
+        ev = events[-1]
+        if (ev.new_value):
+            mode = self.getRelaisModeFromText(ev.source.text)
+            lightControl.setRelaisMode(Relais.LAMP_NORTH, mode)
+
+    @event.connect('detectorYardButtonActive.checked', 
+                   'detectorYardButtonMasked.checked')
+    def radioDetectorYardChanged(self, *events):
+        ev = events[-1]
+        if (ev.new_value):
+            mode = self.getDetectorModeFromText(ev.source.text)
+            lightControl.setDetectorMode(Detector.MOTION_SENSE_SOUTH, mode)
+
+    @event.connect('detectorTerraceButtonActive.checked', 
+                   'detectorTerraceButtonMasked.checked')
+    def radioDetectorTerraceChanged(self, *events):
+        ev = events[-1]
+        if (ev.new_value):
+            mode = self.getDetectorModeFromText(ev.source.text)
+            lightControl.setDetectorMode(Detector.MOTION_SENSE_TERRACE, mode)
+
+    @event.connect('detectorGarageButtonActive.checked', 
+                   'detectorGarageButtonMasked.checked')
+    def radioDetectorGarageChanged(self, *events):
+        ev = events[-1]
+        if (ev.new_value):
+            mode = self.getDetectorModeFromText(ev.source.text)
+            lightControl.setDetectorMode(Detector.MOTION_SENSE_NORTH, mode)
+
+    @event.connect('lampTerraceBrightnessSlider.value')
+    def sliderChangeBrightness(self, *events):
+        ev = events[-1]
+        lightControl.setPwm(Pwm.LAMP_TERRACE, ev.new_value)
+
         
-    class JS:
-
-        @event.emitter
-        def changeLampMode(self, js_event):
-            return dict(lamp=js_event['lamp'], mode=js_event['mode'])
-
-        @event.emitter
-        def changeDetectorMode(self, js_event):
-            return dict(detector=js_event['detector'], mode=js_event['mode'])
-
-        @event.emitter
-        def changeBrightness(self, js_event):
-            return dict(lamp=js_event['lamp'], value=js_event['value'])
-
-        @event.connect('lampYardFrontButtonAuto.checked', 
-                       'lampYardFrontButtonOn.checked',
-                       'lampYardFrontButtonOff.checked')
-        def radioLampYardFrontChanged(self, *events):
-            ev = events[-1]
-            self.changeLampMode({'lamp': 'LampYardFront', 'mode': ev.source.text})
-
-        @event.connect('lampYardRearButtonAuto.checked', 
-                       'lampYardRearButtonOn.checked',
-                       'lampYardRearButtonOff.checked')
-        def radioLampYardRearChanged(self, *events):
-            ev = events[-1]
-            self.changeLampMode({'lamp': 'LampYardRear', 'mode': ev.source.text})
-
-        @event.connect('lampTerraceButtonAuto.checked', 
-                       'lampTerraceButtonOn.checked',
-                       'lampTerraceButtonOff.checked')
-        def radioLampTerraceChanged(self, *events):
-            ev = events[-1]
-            self.changeLampMode({'lamp': 'LampTerrace', 'mode': ev.source.text})
-
-        @event.connect('lampGarageButtonAuto.checked', 
-                       'lampGarageButtonOn.checked',
-                       'lampGarageButtonOff.checked')
-        def radioLampGarageChanged(self, *events):
-            ev = events[-1]
-            self.changeLampMode({'lamp': 'LampGarage', 'mode': ev.source.text})
-
-        @event.connect('detectorYardButtonActive.checked', 
-                       'detectorYardButtonMasked.checked')
-        def radioDetectorYardChanged(self, *events):
-            ev = events[-1]
-            self.changeDetectorMode({'detector': 'DetectorYard', 'mode': ev.source.text})
-
-        @event.connect('detectorTerraceButtonActive.checked', 
-                       'detectorTerraceButtonMasked.checked')
-        def radioDetectorTerraceChanged(self, *events):
-            ev = events[-1]
-            self.changeDetectorMode({'detector': 'DetectorTerrace', 'mode': ev.source.text})
-
-        @event.connect('detectorGarageButtonActive.checked', 
-                       'detectorGarageButtonMasked.checked')
-        def radioDetectorGarageChanged(self, *events):
-            ev = events[-1]
-            self.changeDetectorMode({'detector': 'DetectorGarage', 'mode': ev.source.text})
-
-        @event.connect('lampTerraceBrightnessSlider.value')
-        def sliderChangeBrightness(self, *events):
-            ev = events[-1]
-            self.changeBrightness({'lamp': 'LampTerrace', 'value': ev.new_value})
-
 if __name__ == "__main__":
     
     app.create_server(host="0.0.0.0", port=8080)
