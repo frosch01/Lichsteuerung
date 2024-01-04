d495fb0c60617cce8e2d15b421c18c320fad1460
Ralph Scharpf <ralph.scharpf@mercedes-benz.com>
2024-01-04 18:35:47 +0100

    ✨ Add s0 energy metering
    S0 is a pulse based interface. It is found on many energy meters. The
    implementation is based on a software counter. By derivating the pulses
    over time, the power consumption is calculated.

---
+++ pytest/test_s0_meter.py
@@ -0,0 +1,20 @@
+import mock
+import pytest
+import s0_meter
+from s0_meter import S0Meter
+
+class TestS0Meter:
+    def test_increment(self):
+        with mock.patch.object(s0_meter.time, 'monotonic_ns') as mock_monotonic_ns:
+            mock_monotonic_ns.return_value = 1000000000 # 1s
+            meter = S0Meter()
+            assert meter.total == 0
+            assert meter.last_event == 1000000000
+            meter.pulse(2000000000) # 2s
+            assert meter.total == 1
+            assert meter.last_event == 2000000000
+            mock_monotonic_ns.return_value = 2500000000 # 2.5s, delta 0.5s
+            assert meter.power == 1800
+            assert meter.energy == 0.0005
+            mock_monotonic_ns.return_value = 4000000000 # 4.0s, delta 2s
+            assert meter.power == 900
